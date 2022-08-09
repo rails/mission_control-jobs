11e4d10839cafc93ec975098a0ea5625191dd586
Jorge Manrubia <jorge@hey.com>
2022-08-09 13:01:14 +0200

    This was actually a setting in the generated dummy app

--- app/controllers/mission_control/jobs/application_controller.rb
+++ app/controllers/mission_control/jobs/application_controller.rb
@@ -2,6 +2,8 @@ module MissionControl
   module Jobs
     class ApplicationController < ActionController::Base
       layout "mission_control/jobs/application"
+
+      self.include_all_helpers = true
     end
   end
 end
--- app/helpers/mission_control/jobs/application_helper.rb
+++ app/helpers/mission_control/jobs/application_helper.rb
@@ -1,7 +1,6 @@
 module MissionControl
   module Jobs
     module ApplicationHelper
-      include NavigationHelper
     end
   end
 end
