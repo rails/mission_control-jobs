0fe61a70def6975910f93f989b8e73fed8df720f
Jorge Manrubia <jorge@hey.com>
2022-08-09 11:44:54 +0200

    Move application layout template to the engine (it was in Dummy by mistake)

--- app/controllers/mission_control/jobs/application_controller.rb
+++ app/controllers/mission_control/jobs/application_controller.rb
@@ -1,6 +1,7 @@
 module MissionControl
   module Jobs
     class ApplicationController < ActionController::Base
+      layout "mission_control/jobs/application"
     end
   end
 end
--- app/controllers/mission_control/jobs/queues_controller.rb
+++ app/controllers/mission_control/jobs/queues_controller.rb
@@ -1,4 +1,4 @@
-class MissionControl::Jobs::QueuesController < ApplicationController
+class MissionControl::Jobs::QueuesController < MissionControl::Jobs::ApplicationController
   def index
     @queues = ActiveJob::Base.queues.values
   end
