0d19942a36a32925e92f8445c681ee2d00bb939c
Jorge Manrubia <jorge@hey.com>
2022-08-09 12:50:08 +0200

    List jobs in queue

--- app/controllers/mission_control/jobs/queues_controller.rb
+++ app/controllers/mission_control/jobs/queues_controller.rb
@@ -1,5 +1,16 @@
 class MissionControl::Jobs::QueuesController < MissionControl::Jobs::ApplicationController
+  before_action :set_queue
+
   def index
     @queues = ActiveJob::Base.queues.values
   end
+
+  def show
+    @jobs = @queue.jobs
+  end
+
+  private
+    def set_queue
+      @queue = ActiveJob::Base.queues[params[:id]]
+    end
 end
