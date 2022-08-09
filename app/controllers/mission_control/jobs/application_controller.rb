1cc4bd6a8119171e0337c97aaa78690c6995c721
Jorge Manrubia <jorge@hey.com>
2022-08-09 13:01:57 +0200

    Show page title

--- app/controllers/mission_control/jobs/application_controller.rb
+++ app/controllers/mission_control/jobs/application_controller.rb
@@ -2,8 +2,6 @@ module MissionControl
   module Jobs
     class ApplicationController < ActionController::Base
       layout "mission_control/jobs/application"
-
-      self.include_all_helpers = true
     end
   end
 end
