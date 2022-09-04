f22a955df166c3745db5e45a853cb1ef7a38be72
Jorge Manrubia <jorge@hey.com>
2022-09-04 07:36:51 +0200

    Test that finding servers by name or slug works

--- test/mission_control/jobs/application_test.rb
+++ test/mission_control/jobs/application_test.rb
@@ -13,4 +13,11 @@ class MissionControl::Jobs::ApplicationTest < ActiveSupport::TestCase
     assert_equal "chicago", server.name
     assert_equal queue_adapter, server.queue_adapter
   end
+
+  test "jobs servers can be found by name or slug" do
+    queue_adapter = ActiveJob::QueueAdapters::ResqueAdapter.new
+    @application.add_servers "US east 1": queue_adapter
+    assert_equal queue_adapter, @application.find_server("US east 1").queue_adapter
+    assert_equal queue_adapter, @application.find_server("us-east-1").queue_adapter
+  end
 end
