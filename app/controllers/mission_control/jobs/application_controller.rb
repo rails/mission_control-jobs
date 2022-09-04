07c02f04ccc56a621c472345ea181fee139f42ce
Jorge Manrubia <jorge@hey.com>
2022-09-04 17:53:33 +0200

    Remove dummy action

--- app/controllers/mission_control/jobs/application_controller.rb
+++ app/controllers/mission_control/jobs/application_controller.rb
@@ -1,14 +1,8 @@
 class MissionControl::Jobs::ApplicationController < ActionController::Base
   layout "mission_control/jobs/application"
 
-  before_action :dummy
-
   private
     def default_url_options
       { server_id: MissionControl::Jobs::Current.server }
-    end
-
-    def dummy
-      ActiveJob::Base.current_queue_adapter = MissionControl::Jobs.applications.first.servers.first.queue_adapter
     end
 end
