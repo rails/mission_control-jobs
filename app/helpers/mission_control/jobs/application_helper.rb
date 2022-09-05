61ebae54682cadb1bf22b004015c24deaaa0004e
Jorge Manrubia <jorge@hey.com>
2022-09-05 09:31:10 +0200

    Fix system tests

--- app/helpers/mission_control/jobs/application_helper.rb
+++ app/helpers/mission_control/jobs/application_helper.rb
@@ -1,4 +1,8 @@
 module MissionControl::Jobs
   module ApplicationHelper
+    # Explicit helper inclusion because ApplicationController inherits from the host app.
+    #
+    # We can't rely on +config.action_controller.include_all_helpers = true+ in the host app.
+    include JobsHelper, NavigationHelper
   end
 end
