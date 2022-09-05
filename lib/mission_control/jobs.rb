01d3fb9f24c0ad2a4dcd7aa8b79980818f29f7e3
Jorge Manrubia <jorge@hey.com>
2022-09-05 11:20:56 +0200

    Place requires first

--- lib/mission_control/jobs.rb
+++ lib/mission_control/jobs.rb
@@ -1,12 +1,12 @@
+require "mission_control/jobs/version"
+require "mission_control/jobs/engine"
+
 require "zeitwerk"
 
 loader = Zeitwerk::Loader.new
 loader.inflector = Zeitwerk::GemInflector.new(__FILE__)
 loader.push_dir(File.expand_path("..", __dir__))
 loader.setup
-
-require "mission_control/jobs/version"
-require "mission_control/jobs/engine"
 
 module MissionControl
   module Jobs
