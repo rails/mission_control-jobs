4ef8f9c557db2a52399e1dbfa8e2edced3ff7f2b
Jorge Manrubia <jorge@hey.com>
2022-09-06 22:53:48 +0200

    Add warning dialogs when discarding jobs

--- app/controllers/mission_control/jobs/failed_jobs_controller.rb
+++ app/controllers/mission_control/jobs/failed_jobs_controller.rb
@@ -3,5 +3,6 @@ class MissionControl::Jobs::FailedJobsController < MissionControl::Jobs::Applica
 
   def index
     @jobs = ApplicationJob.jobs.failed
+    @jobs_count = @jobs.count # Capturing because used in several places and queries are expensive for redis instances in remote datacenters.
   end
 end
