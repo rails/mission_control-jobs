222b0d54f7ae0f9d01f0eb158d0038d65bd79b1c
Jorge Manrubia <jorge@hey.com>
2022-09-23 18:58:15 +0200

    Offer an opportunity for optimized "discard all" operation.

--- app/controllers/mission_control/jobs/failed_jobs/bulk_discards_controller.rb
+++ app/controllers/mission_control/jobs/failed_jobs/bulk_discards_controller.rb
@@ -2,9 +2,19 @@ class MissionControl::Jobs::FailedJobs::BulkDiscardsController < MissionControl:
   include MissionControl::Jobs::FailedJobFiltering
 
   def create
-    jobs_to_discard_count = bulk_limited_filtered_failed_jobs.count
-    bulk_limited_filtered_failed_jobs.discard_all
+    jobs_to_discard_count = jobs_to_discard.count
+    jobs_to_discard.discard_all
 
     redirect_to application_failed_jobs_url(@application), notice: "Discarded #{jobs_to_discard_count} jobs"
   end
+
+  private
+    def jobs_to_discard
+      if @job_class_filter.present?
+        bulk_limited_filtered_failed_jobs
+      else
+        # we don't want to apply any limit since "discarding all" without parameters can be optimized in the adapter as a much faster operation
+        ApplicationJob.jobs.failed
+      end
+    end
 end
