08c0238d9ae1d3e7473f88d64e6d3d4b6f3ba766
Jorge Manrubia <jorge@hey.com>
2022-09-23 16:51:54 +0200

    Limit the max number of jobs we execute

--- app/controllers/concerns/mission_control/jobs/failed_job_filtering.rb
+++ app/controllers/concerns/mission_control/jobs/failed_job_filtering.rb
@@ -1,6 +1,8 @@
 module MissionControl::Jobs::FailedJobFiltering
   extend ActiveSupport::Concern
 
+  MAX_NUMBER_OF_JOBS_FOR_BULK_OPERATIONS = 3000
+
   included do
     before_action :set_job_class_filter
   end
@@ -12,5 +14,11 @@ module MissionControl::Jobs::FailedJobFiltering
 
     def filtered_failed_jobs
       ApplicationJob.jobs.failed.where(job_class: @job_class_filter)
+    end
+
+    # We set a hard limit to prevent overloading redis. This should be enough for most scenarios. For
+    # cases where we need to retry a huge sets of jobs, we offer a runbook that uses the new API.
+    def bulk_limited_filtered_failed_jobs
+      filtered_failed_jobs.limit(MAX_NUMBER_OF_JOBS_FOR_BULK_OPERATIONS)
     end
 end
--- test/system/paginate_jobs_test.rb
+++ test/system/paginate_jobs_test.rb
@@ -22,7 +22,10 @@ class PaginateJobsTest < ApplicationSystemTestCase
     def assert_jobs(range)
       expected_indexes = range.to_a
 
-      assert_text /FailingJob.*#{expected_indexes.first}/i # Wait for page to load
+      # Wait for page to load
+      assert_text /FailingJob.*#{expected_indexes.first}/i
+      assert_text /FailingJob.*#{expected_indexes.last}/i
+
       assert_equal expected_indexes.length, job_row_elements.length
 
       job_row_elements.each.with_index do |job_element, index|
