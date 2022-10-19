3e3c00a72c94149aa38c269da6a4a32b101664b6
Jorge Manrubia <jorge@hey.com>
2022-10-19 15:50:52 +0200

    Use more modern syntax for building HTML tags

--- app/helpers/mission_control/jobs/dates_helper.rb
+++ app/helpers/mission_control/jobs/dates_helper.rb
@@ -1,5 +1,5 @@
 module MissionControl::Jobs::DatesHelper
   def time_ago_in_words_with_title(time)
-    content_tag(:time, time_ago_in_words(time), title: time.to_fs(:long))
+    tag.span time_ago_in_words(time), title: time.to_fs(:long)
   end
 end
