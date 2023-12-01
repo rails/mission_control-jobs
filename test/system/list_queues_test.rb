db06cf236050e82daccb928b1548a2a24e1e44a1
Rosa Gutierrez <rosa@37signals.com>
2023-12-01 18:27:52 +0100

    Filter by configured queue name prefix when fetching queues
    That's it, by active_job.config.queue_name_prefix. This is useful when
    an adapter has queues from different environments mixed together.

--- test/system/list_queues_test.rb
+++ test/system/list_queues_test.rb
@@ -3,11 +3,11 @@ require_relative "../application_system_test_case"
 class ListQueuesTest < ApplicationSystemTestCase
   setup do
     create_queues *10.times.collect { |index| "queue_#{index}" }
-
-    visit queues_path
   end
 
   test "list queues sorted by name" do
+    visit queues_path
+
     assert_equal 10, queue_row_elements.length
     queue_row_elements.each.with_index do |queue_element, index|
       within queue_element do
@@ -15,4 +15,23 @@ class ListQueuesTest < ApplicationSystemTestCase
       end
     end
   end
+
+  test "list queues sorted by name and filtered by prefix" do
+    with_queue_name_prefix do
+      visit queues_path
+
+      assert_equal 1, queue_row_elements.length
+      within queue_row_elements.first do
+        assert_text "queue_1"
+      end
+    end
+  end
+
+  private
+    def with_queue_name_prefix(&block)
+      previous_prefix, ActiveJob::Base.queue_name_prefix = ActiveJob::Base.queue_name_prefix, "queue_1"
+      yield
+    ensure
+      ActiveJob::Base.queue_name_prefix = previous_prefix
+    end
 end
