b2c04ff1fdf11afcd2dcf5342f2d6b6fadda2e0f
Rosa Gutierrez <rosa@37signals.com>
2024-01-22 09:55:57 +0100

    Stop returning a 500 error when a job is not found

--- app/controllers/mission_control/jobs/application_controller.rb
+++ app/controllers/mission_control/jobs/application_controller.rb
@@ -3,6 +3,10 @@ class MissionControl::Jobs::ApplicationController < MissionControl::Jobs.base_co
 
   include MissionControl::Jobs::ApplicationScoped
 
+  rescue_from(ActiveJob::Errors::JobNotFoundError) do |error|
+    redirect_to root_path, alert: error.message
+  end
+
   private
     def default_url_options
       { server_id: MissionControl::Jobs::Current.server }
