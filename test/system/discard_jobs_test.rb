49b76645cebd6d08fd4c9124825ace71f21cfaae
Rosa Gutierrez <rosa@37signals.com>
2024-01-24 20:50:25 +0100

    Add some more tests for filtering by queue

--- test/system/discard_jobs_test.rb
+++ test/system/discard_jobs_test.rb
@@ -2,26 +2,27 @@ require_relative "../application_system_test_case"
 
 class DiscardJobsTest < ApplicationSystemTestCase
   setup do
-    5.times { |index| FailingJob.perform_later(index) }
-    5.times { |index| FailingReloadedJob.perform_later(5 + index) }
+    4.times { |index| FailingJob.set(queue: "queue_1").perform_later(index) }
+    3.times { |index| FailingReloadedJob.set(queue: "queue_2").perform_later(4 + index) }
+    2.times { |index| FailingJob.set(queue: "queue_2").perform_later(7 + index) }
     perform_enqueued_jobs
 
     visit jobs_path(:failed)
   end
 
   test "discard all failed jobs" do
-    assert_equal 10, job_row_elements.length
+    assert_equal 9, job_row_elements.length
 
     accept_confirm do
       click_on "Discard all"
     end
 
-    assert_text "Discarded 10 jobs"
+    assert_text "Discarded 9 jobs"
     assert_empty job_row_elements
   end
 
   test "discard a single job" do
-    assert_equal 10, job_row_elements.length
+    assert_equal 9, job_row_elements.length
     expected_job_id = ApplicationJob.jobs.failed[2].job_id
 
     within_job_row "2" do
@@ -32,13 +33,27 @@ class DiscardJobsTest < ApplicationSystemTestCase
 
     assert_text "Discarded job with id #{expected_job_id}"
 
-    assert_equal 9, job_row_elements.length
+    assert_equal 8, job_row_elements.length
   end
 
-  test "retry a selection of filtered jobs" do
-    assert_equal 10, job_row_elements.length
+  test "discard a selection of filtered jobs by class name" do
+    assert_equal 9, job_row_elements.length
 
-    fill_in "filter[job_class_name]", with: "FailingJob"
+    fill_in "filter[job_class_name]", with: "FailingReloadedJob"
+    assert_text /3 jobs selected/i
+
+    accept_confirm do
+      click_on "Discard selection"
+    end
+
+    assert_text /discarded 3 jobs/i
+    assert_equal 6, job_row_elements.length
+  end
+
+  test "discard a selection of filtered jobs by queue name" do
+    assert_equal 9, job_row_elements.length
+
+    fill_in "filter[queue_name]", with: "queue_2"
     assert_text /5 jobs selected/i
 
     accept_confirm do
@@ -46,11 +61,11 @@ class DiscardJobsTest < ApplicationSystemTestCase
     end
 
     assert_text /discarded 5 jobs/i
-    assert_equal 5, job_row_elements.length
+    assert_equal 4, job_row_elements.length
   end
 
   test "discard a job from its details screen" do
-    assert_equal 10, job_row_elements.length
+    assert_equal 9, job_row_elements.length
     failed_job = ApplicationJob.jobs.failed[2]
     visit job_path(failed_job.job_id)
 
@@ -59,6 +74,6 @@ class DiscardJobsTest < ApplicationSystemTestCase
     end
 
     assert_text "Discarded job with id #{failed_job.job_id}"
-    assert_equal 9, job_row_elements.length
+    assert_equal 8, job_row_elements.length
   end
 end
