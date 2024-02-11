0acad6931119933b8367b1b922e8d30ca2e9c3c8
Gary Tou <gary@garytou.com>
2024-02-10 20:54:45 -0800

    Add test for existing `ActiveJob#failed?` method

--- test/active_job/queue_adapters/adapter_testing/job_statuses.rb
+++ test/active_job/queue_adapters/adapter_testing/job_statuses.rb
@@ -2,6 +2,18 @@ module ActiveJob::QueueAdapters::AdapterTesting::JobStatuses
   extend ActiveSupport::Concern
   extend ActiveSupport::Testing::Declarative
 
+  test "job is failed?" do
+    skip_unless_queue_adapter_supports_status :failed
+
+    job = FailingJob.perform_later
+    assert_not job.failed?
+
+    perform_enqueued_jobs
+    job = ActiveJob.jobs.failed.last
+
+    assert job.failed?
+  end
+
   test "job is scheduled_enqueue_delayed?" do
     skip_unless_queue_adapter_supports_status :scheduled
 
