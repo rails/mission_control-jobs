2a358d3799de69d233b08ca2ad7ab7609985d71f
Juan Vasquez <juan@ombulabs.com>
2024-02-18 23:07:56 -0600

    Titleize navigation status tab

--- app/helpers/mission_control/jobs/navigation_helper.rb
+++ app/helpers/mission_control/jobs/navigation_helper.rb
@@ -4,7 +4,7 @@ module MissionControl::Jobs::NavigationHelper
   def navigation_sections
     { queues: [ "Queues", application_queues_path(@application) ] }.tap do |sections|
       supported_job_statuses.without(:pending).each do |status|
-         sections[navigation_section_for_status(status)] = [ "#{status.to_s.titleize} jobs (#{jobs_count_with_status(status)})", application_jobs_path(@application, status) ]
+         sections[navigation_section_for_status(status)] = [ status_title_tab(status), application_jobs_path(@application, status) ]
       end
 
       sections[:workers] = [ "Workers", application_workers_path(@application) ] if workers_exposed?
@@ -47,5 +47,9 @@ module MissionControl::Jobs::NavigationHelper
   def jobs_count_with_status(status)
     count = ApplicationJob.jobs.with_status(status).count
     count.infinite? ? "..." : number_to_human(count)
+  end
+
+  def status_title_tab(status)
+    "#{status.to_s.titleize} Jobs (#{jobs_count_with_status(status)})"
   end
 end
