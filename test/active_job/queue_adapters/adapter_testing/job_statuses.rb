017120105e4db84b7bb9dec1145f880f7dfa1d8d
Gary Tou <gary@garytou.com>
2024-02-19 17:30:24 -0800

    Remove adapter tests for `scheduled_enqueue_delayed?`

--- test/active_job/queue_adapters/adapter_testing/job_statuses.rb
+++ test/active_job/queue_adapters/adapter_testing/job_statuses.rb
@@ -13,36 +13,4 @@ module ActiveJob::QueueAdapters::AdapterTesting::JobStatuses
 
     assert job.failed?
   end
-
-  test "job is scheduled_enqueue_delayed?" do
-    skip_unless_queue_adapter_supports_status :scheduled
-
-    DummyJob.set(wait: 5.minute).perform_later
-    job = ActiveJob.jobs.scheduled.last
-    refute job.scheduled_enqueue_delayed?
-
-    travel 5.minute + MissionControl::Jobs.scheduled_job_delay_threshold + 1.second
-
-    assert job.scheduled_enqueue_delayed?
-  end
-
-  test "job with configured scheduled_job_delay_threshold is scheduled_enqueue_delayed?" do
-    skip_unless_queue_adapter_supports_status :scheduled
-
-    begin
-      original_threshold = MissionControl::Jobs.scheduled_job_delay_threshold
-      MissionControl::Jobs.scheduled_job_delay_threshold = 3.minutes
-
-      DummyJob.set(wait: 5.minute).perform_later
-      job = ActiveJob.jobs.scheduled.last
-      refute job.scheduled_enqueue_delayed?
-
-      travel 5.minute + MissionControl::Jobs.scheduled_job_delay_threshold + 1.second
-
-      assert job.scheduled_enqueue_delayed?
-    ensure
-      MissionControl::Jobs.scheduled_job_delay_threshold = original_threshold
-    end
-  end
-
 end
