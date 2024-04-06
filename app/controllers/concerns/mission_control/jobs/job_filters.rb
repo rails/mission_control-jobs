2e4291d8f11cc659e56989afab4893506c6052bd
Ezequiel Salas <salaseze@gmail.com>
2024-04-05 21:57:10 -0300

    added timezone

--- app/controllers/concerns/mission_control/jobs/job_filters.rb
+++ app/controllers/concerns/mission_control/jobs/job_filters.rb
@@ -11,7 +11,12 @@ module MissionControl::Jobs::JobFilters
     def set_filters
       @job_filters = { job_class_name: params.dig(:filter, :job_class_name).presence, queue_name: params.dig(:filter, :queue_name).presence,
                        finished_at_start: params.dig(:filter, :finished_at_start).presence, finished_at_end: params.dig(:filter, :finished_at_end) }.compact
+      finished_at_timezoned_filters
     end
+
+    def finished_at_timezoned_filters
+      @job_filters[:finished_at_start] = DateTime.parse(@job_filters[:finished_at_start]).in_time_zone if @job_filters[:finished_at_start].present?
+      @job_filters[:finished_at_end] = DateTime.parse(@job_filters[:finished_at_end]).in_time_zone if @job_filters[:finished_at_end].present?
     end
 
     def active_filters?
