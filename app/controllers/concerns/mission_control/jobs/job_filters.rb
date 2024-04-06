b4692060ae8d0fb4d479fa9a56ab610513c7455a
Ezequiel Salas <salaseze@gmail.com>
2024-04-05 21:56:24 -0300

    added filters to _filters partial and params

--- app/controllers/concerns/mission_control/jobs/job_filters.rb
+++ app/controllers/concerns/mission_control/jobs/job_filters.rb
@@ -9,7 +9,9 @@ module MissionControl::Jobs::JobFilters
 
   private
     def set_filters
-      @job_filters = { job_class_name: params.dig(:filter, :job_class_name).presence, queue_name: params.dig(:filter, :queue_name).presence }.compact
+      @job_filters = { job_class_name: params.dig(:filter, :job_class_name).presence, queue_name: params.dig(:filter, :queue_name).presence,
+                       finished_at_start: params.dig(:filter, :finished_at_start).presence, finished_at_end: params.dig(:filter, :finished_at_end) }.compact
+    end
     end
 
     def active_filters?
