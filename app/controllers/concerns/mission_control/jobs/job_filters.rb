62d5b3b24bc77bf358dd64d374f17f47b859c416
Ezequiel Salas <salaseze@gmail.com>
2024-06-10 09:05:31 -0300

    enhacements set filters

--- app/controllers/concerns/mission_control/jobs/job_filters.rb
+++ app/controllers/concerns/mission_control/jobs/job_filters.rb
@@ -10,16 +10,15 @@ module MissionControl::Jobs::JobFilters
   private
     def set_filters
       @job_filters = { job_class_name: params.dig(:filter, :job_class_name).presence, queue_name: params.dig(:filter, :queue_name).presence,
-                       finished_at_start: params.dig(:filter, :finished_at_start).presence, finished_at_end: params.dig(:filter, :finished_at_end) }.compact
-      finished_at_timezoned_filters
-    end
-
-    def finished_at_timezoned_filters
-      @job_filters[:finished_at_start] = DateTime.parse(@job_filters[:finished_at_start]).in_time_zone if @job_filters[:finished_at_start].present?
-      @job_filters[:finished_at_end] = DateTime.parse(@job_filters[:finished_at_end]).in_time_zone if @job_filters[:finished_at_end].present?
+                       finished_at_start: date_with_time_zone(params.dig(:filter, :finished_at_start).presence), finished_at_end: date_with_time_zone(params.dig(:filter, :finished_at_end).presence) }.compact
     end
 
     def active_filters?
       @job_filters.any?
+    end
+
+    def date_with_time_zone(date)
+      return date if date.nil?
+      DateTime.parse(date).in_time_zone
     end
 end
