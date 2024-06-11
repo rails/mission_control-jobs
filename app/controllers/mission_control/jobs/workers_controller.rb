25404db764b32734f37e0dab9126237b99037da6
Ezequiel Salas <salaseze@gmail.com>
2024-06-11 18:25:31 -0300

    changed name to pid in controller + views

--- app/controllers/mission_control/jobs/workers_controller.rb
+++ app/controllers/mission_control/jobs/workers_controller.rb
@@ -37,6 +37,6 @@ class MissionControl::Jobs::WorkersController < MissionControl::Jobs::Applicatio
     end
 
     def set_filters
-      @worker_filters = { hostname: params.dig(:filter, :hostname).presence, name: params.dig(:filter, :name) }.compact
+      @worker_filters = { hostname: params.dig(:filter, :hostname).presence, pid: params.dig(:filter, :pid) }.compact
     end
 end
