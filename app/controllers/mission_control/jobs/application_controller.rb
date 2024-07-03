0011d2e84ceb23c7fee8bcfa461df2fbe8db180b
Manuel van Rijn <manuel@manuelvanrijn.nl>
2024-07-03 14:35:23 +0200

    Enforce the locale to `en`

--- app/controllers/mission_control/jobs/application_controller.rb
+++ app/controllers/mission_control/jobs/application_controller.rb
@@ -4,8 +4,14 @@ class MissionControl::Jobs::ApplicationController < MissionControl::Jobs.base_co
   include MissionControl::Jobs::ApplicationScoped, MissionControl::Jobs::NotFoundRedirections
   include MissionControl::Jobs::AdapterFeatures
 
+  around_action :set_current_locale
+
   private
     def default_url_options
       { server_id: MissionControl::Jobs::Current.server }
+    end
+
+    def set_current_locale(&block)
+      I18n.with_locale(:en, &block)
     end
 end
