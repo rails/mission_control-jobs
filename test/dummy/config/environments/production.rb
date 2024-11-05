3dffef652b7dfc46caf0842b640ac0ce9aa3a03c
Rosa Gutierrez <rosa@37signals.com>
2024-11-05 19:04:58 +0100

    Drop Rails as a full dependency, only require some of the frameworks

--- test/dummy/config/environments/production.rb
+++ test/dummy/config/environments/production.rb
@@ -37,9 +37,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
@@ -63,12 +60,6 @@ Rails.application.configure do
   # config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # config.active_job.queue_name_prefix = "dummy_production"
-
-  config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
