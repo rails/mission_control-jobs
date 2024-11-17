38b3bf355f763f055767773cacf015f11fd342da
Ezequiel Salas <salaseze@gmail.com>
2024-11-17 16:58:38 -0300

    god bye presence

--- app/controllers/concerns/mission_control/jobs/job_filters.rb
+++ app/controllers/concerns/mission_control/jobs/job_filters.rb
@@ -10,13 +10,14 @@ module MissionControl::Jobs::JobFilters
   private
     def set_filters
       @job_filters = { job_class_name: params.dig(:filter, :job_class_name).presence, queue_name: params.dig(:filter, :queue_name).presence,
-                       finished_at: date_with_time_zone(params.dig(:filter, :finished_at_start).presence)..date_with_time_zone(params.dig(:filter, :finished_at_end).presence) }.compact
+                       finished_at: date_with_time_zone(params.dig(:filter, :finished_at_start))..date_with_time_zone(params.dig(:filter, :finished_at_end)) }.compact
     end
 
     def active_filters?
       @job_filters.any?
     end
 
+    # TODO: move to helpers ?
     def date_with_time_zone(date)
       if date.present?
         DateTime.parse(date).in_time_zone
