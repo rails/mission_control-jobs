de4116c71ca56ac4f014ee87683255d49d596812
Rosa Gutierrez <rosa@37signals.com>
2024-12-02 22:28:03 +0100

    Simplify time helpers
    One of them is basically the same as another one.

--- app/helpers/mission_control/jobs/dates_helper.rb
+++ app/helpers/mission_control/jobs/dates_helper.rb
@@ -1,10 +1,6 @@
 module MissionControl::Jobs::DatesHelper
-  def time_ago_in_words_with_title(time)
-    tag.span time_ago_in_words(time), title: time.to_fs(:long)
-  end
-
   def time_distance_in_words_with_title(time)
-    tag.span distance_of_time_in_words_to_now(time, include_seconds: true), title: "Since #{time.to_fs(:long)}"
+    tag.span time_ago_in_words(time, include_seconds: true), title: "Since #{time.to_fs(:long)}"
   end
 
   def bidirectional_time_distance_in_words_with_title(time)
@@ -15,9 +11,5 @@ module MissionControl::Jobs::DatesHelper
     end
 
     tag.span time_distance, title: time.to_fs(:long)
-  end
-
-  def formatted_time(time)
-    time.in_time_zone.strftime("%Y-%m-%d %H:%M:%S.%3N %Z")
   end
 end
