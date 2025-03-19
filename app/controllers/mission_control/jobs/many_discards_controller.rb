6497bdebd351fa14c021cd605ff9e1cd7a929366
Andrew <andrew.scullion@fullscript.com>
2025-03-19 15:10:47 -0400

    Improved error handling

--- app/controllers/mission_control/jobs/many_discards_controller.rb
+++ app/controllers/mission_control/jobs/many_discards_controller.rb
@@ -1,7 +1,8 @@
 class MissionControl::Jobs::ManyDiscardsController < MissionControl::Jobs::ApplicationController
   def create
-    params[:job_ids].each do |job_id|
-      ActiveJob.jobs.find_by_id!(job_id).discard
-    end
+    discarded_jobs = params[:job_ids].map do |job_id|
+      ActiveJob.jobs.find_by_id(job_id)&.discard
+    end.compact
+    redirect_to request.referer, notice: "Discarded #{discarded_jobs.size} of #{params[:job_ids].size} selected jobs"
   end
 end
--- app/controllers/mission_control/jobs/many_retries_controller.rb
+++ app/controllers/mission_control/jobs/many_retries_controller.rb
@@ -1,9 +1,9 @@
 class MissionControl::Jobs::ManyRetriesController < MissionControl::Jobs::ApplicationController
-  # include MissionControl::Jobs::FailedJobsBulkOperations
-
   def create
-    params[:job_ids].each do |job_id|
-      ActiveJob.jobs.find_by_id!(job_id).retry
-    end
+    retried_jobs = params[:job_ids].map do |job_id|
+      job = ActiveJob.jobs.find_by_id(job_id)
+      job.retry if job&.failed?
+    end.compact
+    redirect_to request.referer, notice: "Retried #{retried_jobs.size} of #{params[:job_ids].size} selected jobs"
   end
 end
